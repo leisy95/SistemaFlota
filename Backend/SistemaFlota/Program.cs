223c915490514440bfe613f16800d1d2386ce3d8
Leisy <leisy95.ladino@gmail.com>
2026-04-29 17:22:50 -0500

    Iniciando Proyecto

---
+++ Backend/SistemaFlota/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaFlota.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Controllers
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// CORS (Angular)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// MySQL
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+var app = builder.Build();
+
+// Swagger
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// IMPORTANTE: CORS antes de Authorization
+app.UseCors("AngularPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,15 @@
+#  Cómo clonar este proyecto
+
+## 1. Clonar repositorio
+
+```bash
+https://github.com/leisy95/SistemaFlota.git
+
+## 2. Acceder a la carpeta
+-- cd SistemaFlota
+
+## 3. Installar Dependencias
+-- npm install
+
+## 4. Ejecutar el Frontend
+-- ng serve
